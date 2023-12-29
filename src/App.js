36db6bb7a3c9174b48f1e26080a46d058c8328fb
Timka <timkaanddoge@gmail.com>
2023-12-29 21:39:40 +0200

    page-person, I did page and awards

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Book from './pages/Book';
 import Header from './components/header/Header';
 import Email from './components/email/Email';
 import Footer from './components/footer/Footer';
+import PagePerson from './pages/PagePerson';
 
 const url = 'https://www.googleapis.com/books/v1/volumes?q=education&maxResults=40&filter=ebooks&key=AIzaSyCuF3fjVbdjpRNwBz3TGavhUJ5axwWORRA'
 const url2 = 'https://www.googleapis.com/books/v1/volumes?q=fantacy&maxResults=40&filter=ebooks&key=AIzaSyCuF3fjVbdjpRNwBz3TGavhUJ5axwWORRA'
@@ -35,7 +36,8 @@ function App() {
         <main className="main">
           {/* <Home /> */}
           {/* <Library /> */}
-          <Book />
+          {/* <Book /> */}
+          <PagePerson />
         </main>
         <Email />
         <Footer />
